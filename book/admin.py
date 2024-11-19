eac18a781775ae1cbecc77bdfff1b0cb0ab569f6
Devin Liang <devinliang@gmail.com>
2024-11-19 17:45:33 +0800

    Update admin.py

--- book/admin.py
+++ book/admin.py
@@ -4,4 +4,11 @@ from django.contrib import admin
 from book.models import book
 
 # 將資料表註冊到後台管理
-admin.site.register(book)
+class bookAdmin(admin.ModelAdmin):
+    list_display = ( 'pubdate', 'publisher', 'bookname', 'author', 'price')
+    list_filter = ( 'author', 'publisher')
+    search_fields = (['bookname'])
+    ordering = (['pubdate'])
+
+# 將資料表註冊到後台管理
+admin.site.register(book, bookAdmin)
