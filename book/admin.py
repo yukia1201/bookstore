a13c6f54f51b7f3e45924196e0e22e8bc7f2709b
Devin Liang <devinliang@gmail.com>
2024-11-19 17:30:02 +0800

    建立model

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from book.models import book
+
+# 將資料表註冊到後台管理
+admin.site.register(book)
--- book/models.py
+++ book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# 資料表建議使用單數名稱
+class book(models.Model):
+    bookname  = models.CharField(max_length=20, null=False, verbose_name='書名')
+    author    = models.CharField(max_length=20, null=False, verbose_name='作者')
+    publisher = models.CharField(max_length=20, verbose_name='出版社')
+    pubdate   = models.DateField(verbose_name='出版日期')
+    price     = models.IntegerField(verbose_name='定價')
+    content   = models.TextField(verbose_name='詳細內容')
+
+    def __str__(self):
+        return self.bookname +'  '+ self.author
